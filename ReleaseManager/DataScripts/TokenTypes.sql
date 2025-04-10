71dcc486edd0e5096f65c65ed39112dcf5a89f07
Ryan Roethle <ryanroethle7474@gmail.com>
2025-04-10 12:36:34 -0500

    Added Additional TokenTypes.

--- ReleaseManager/DataScripts/TokenTypes.sql
+++ ReleaseManager/DataScripts/TokenTypes.sql
@@ -2,6 +2,33 @@ INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, Up
 VALUES 
 (1, 'RefreshToken', 'Token used for refresh', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
 
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(2, 'AccessToken', 'Short-lived token used for API authentication', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(3, 'AzureDevOpsToken', 'Token for Azure DevOps integration', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(4, 'EmailVerification', 'Token used to verify user email addresses', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(5, 'PasswordReset', 'Token used for password reset requests', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(6, 'InvitationToken', 'Token used for inviting users to organizations', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(7, 'ApiAccessToken', 'Long-lived token for external service integration', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(8, 'TwoFactorAuth', 'Token used for two-factor authentication', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
 
 
 Insert Into TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
