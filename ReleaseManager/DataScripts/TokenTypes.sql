6d282c8030af65852ec9f3bcec75dce0ab47c446
Ryan Roethle <ryanroethle7474@gmail.com>
2025-04-14 11:15:55 -0500

    Added entra token types.

--- ReleaseManager/DataScripts/TokenTypes.sql
+++ ReleaseManager/DataScripts/TokenTypes.sql
@@ -34,3 +34,12 @@ VALUES
 Insert Into TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
 Values(9, 'OauthRefreshToken', 'Long-lived token used to obtain new access tokens', GetDate(),
 '09072755-8B7D-49BB-B967-12598B091971',GetDate(),'09072755-8B7D-49BB-B967-12598B091971')
+
+
+Insert Into TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+Values(10, 'EntraOAuthToken', 'Microsoft Entra OAuth token for Azure DevOps', GetDate(),
+'09072755-8B7D-49BB-B967-12598B091971',GetDate(),'09072755-8B7D-49BB-B967-12598B091971')
+
+INSERT INTO TokenType(Id, Name, Description, CreatedOn, CreatedBy, UpdatedOn, UpdatedBy)
+VALUES 
+(11, 'EntraOAuthRefreshToken', 'Microsoft Entra OAuth refresh token for Azure DevOps', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971', GETUTCDATE(), '09072755-8B7D-49BB-B967-12598B091971')
